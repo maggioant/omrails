bded162da44c892ddeabcffe12b2baee692ef26d
Anthony Maggio <maggioant@gmail.com>
2013-09-08 00:12:52 -0700

    Removed Presence Required on Attachment

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -5,7 +5,7 @@ class Pin < ActiveRecord::Base
   validates :user_id, presence: true
   
   has_attached_file :image, styles: { medium: "320x240>"}
-  validates_attachment :image, presence: true,
+  validates_attachment :image, 
                             content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
                             size: { less_than: 5.megabytes }
   
